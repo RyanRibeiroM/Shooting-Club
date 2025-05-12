231740d2d57c393f3479bedde10df9d4859796d3
RyanRibeiroM <159050423+RyanRibeiroM@users.noreply.github.com>
2025-05-12 20:36:18 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/ShootingClub.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShootingClub.Communication.Requests;
+using ShootingClub.Communication.Responses;
+
+namespace ShootingClub.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
