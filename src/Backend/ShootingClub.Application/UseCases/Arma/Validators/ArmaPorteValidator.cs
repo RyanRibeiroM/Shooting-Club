02c1a4754fbc72b4536d3d66342d736a78dcff34
RyanRibeiroM <159050423+RyanRibeiroM@users.noreply.github.com>
2025-07-19 14:51:51 -0300

    fix: fixing user registration.

--- src/Backend/ShootingClub.Application/UseCases/Arma/Validators/ArmaPorteValidator.cs
+++ src/Backend/ShootingClub.Application/UseCases/Arma/Validators/ArmaPorteValidator.cs
@@ -17,11 +17,11 @@ namespace ShootingClub.Application.UseCases.Arma.Validators
                 .NotEmpty()
                 .GreaterThan(DateOnly.FromDateTime(DateTime.Today)).WithMessage(ResourceMessagesException.VALIDADE_CERTIFICADO_INVALIDA);
 
-            When(arma => arma.DataCertificacao.HasValue, () =>
-            {
-                RuleFor(arma => arma.DataCertificacao)
-                    .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today)).WithMessage(ResourceMessagesException.DATA_CERTIFICADO_INVALIDA);
-            });
+            RuleFor(arma => arma.DataCertificacao)
+                .NotEmpty()
+                .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today)).WithMessage(ResourceMessagesException.DATA_CERTIFICADO_INVALIDA);
+
+
         }
     }
 }
